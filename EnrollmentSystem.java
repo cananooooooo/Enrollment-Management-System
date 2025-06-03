dfad7acb2df2f4abb8707cbf89b8de342038e654
Gloria Cañano <cananogloria2002@gmail.com>
2025-06-04 07:28:26 +0800

    Add files via upload

---
+++ EnrollmentSystem.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package EnrollmentSystem;
+
+/**
+ *
+ * @author Gloria Cañano
+ */
+public class EnrollmentSystem {
+
+    public static void main(String[] args) {
+       javax.swing.SwingUtilities.invokeLater(() -> {
+          new HomePage().setVisible(true);
+      });
+    }
+}
